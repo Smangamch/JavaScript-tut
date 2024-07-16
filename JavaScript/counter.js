bd6a1ef3aaf9f8e969546e467d41d1f3ad5000b7
Smangamch <113479264+Smangamch@users.noreply.github.com>
2024-07-16 12:23:31 +0200

    Add files via upload

--- JavaScript/counter.js
+++ JavaScript/counter.js
@@ -1,20 +1,17 @@
 
 if(!localStorage.getItem('counter')) {
-    localStorage.setItem('counter, 0') 
+    localStorage.setItem('counter', 0) 
 }
 
 
 function count(){      
     let counter = localStorage.getItem('counter') 
     counter++;                                          /*Alternatively we can write "counter += 1" | "counter = counter + 1" */      
-    document.querySelector('h1').innerHTML = counter; 
-    localStorage.setItem('counter', counter)       
-    if(counter % 10 === 0) {
-        alert(`Counter limit ${counter} is reached!`) 
-            }
-        document.addEventListener('DOMContentLoaded', function() {
-            document.querySelector('h1').innerHTML = localStorage.getItem('counter');
-            document.querySelector('button').onclick = count; 
+    document.querySelector('#header').innerHTML = counter; 
+    localStorage.setItem('counter', counter)  
+    }
+    document.addEventListener('DOMContentLoaded', function(){
+        document.querySelector('#header').innerHTML = localStorage.getItem('counter');
+        document.querySelector('button').onclick = count; 
     })
     
-}
